97865bdcffbea2a849c8129a27175aff05a733e0
lucianocabelloo <you@example.com>
2024-02-09 00:20:52 -0300

    Add lat and lon

--- src/Components/SearchListWeather.jsx
+++ src/Components/SearchListWeather.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-export const SearchListWeather = ({ciudad, resultados, lenguaje}) => {
+export const SearchListWeather = ({ ciudad, resultados, lenguaje }) => {
 
-    const API_KEY = "fd2e5b9da6c6cb6b46a6863fc911ca7c"
+    const [Lat, setLat] = useState("")
+    const [Lon, setLon] = useState("")
 
-    const fetchData = async () => {
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${API_KEY}`)
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}&limit=${resultados}&lang=${lenguaje}&appid=4b56f215333470bc78211cf09c98602e
+`);
+                const data = await response.json();
+                if (data && data.length > 0) {
+                    setLat(data[0].lat);
+                    setLon(data[0].lon);
+                } else {
+                    console.error('La respuesta de la API está vacía o no contiene datos válidos.');
+                }
+            } catch (error) {
+                console.error('Error al obtener datos del clima:', error);
+            }
+        };
 
-  return (
-    <div>SearchListWeather</div>
-  )
-}
+        fetchData();
+    }, [ciudad, resultados, lenguaje]);
+
+
+    console.log(Lat)
+    console.log(Lon)
+
+    return (
+        <div>SearchListWeather</div>
+    );
+};
