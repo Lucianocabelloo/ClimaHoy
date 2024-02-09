755be44be9f0cc05bc932e901425b02db6fbba96
lucianocabelloo <you@example.com>
2024-02-09 14:32:28 -0300

    NavSearch Maquetado end

--- src/Components/SearchWeather.jsx
+++ src/Components/SearchWeather.jsx
@@ -30,19 +30,22 @@ export const SearchWeather = () => {
 
   return (
     <>
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form className='d-flex justify-content-end align-content-end gap-3' onSubmit={handleSubmit(onSubmit)}>
 
-    <Form.Group className="mb-3">
-      <Form.Label>Ingrese Ciudad</Form.Label>
+    <Form.Group className="mb-3 d-flex flex-column gap-3">
+      <div className='d-flex gap-3'>
+      <Button id='buttonSearch'><i class="bi bi-search"></i></Button>
       <Form.Control 
       placeholder="Ingrese nombre o codigo de ciudad"
       type='text'
       id='inputSearch'
+      className="custom-placeholder"
       {...register("Ciudad")}
       />
+
+      </div>
     </Form.Group>
     <Form.Group className="mb-3">
-      <Form.Label>Numero de Resultados</Form.Label>
       <Form.Select
       {...register("Resultados")}
       id="inputSelect" >
@@ -52,7 +55,6 @@ export const SearchWeather = () => {
       </Form.Select>
     </Form.Group>
     <Form.Group className="mb-3">
-      <Form.Label>Elige Lenguaje</Form.Label>
       <Form.Select
       {...register("Lenguaje")}
       id="inputLenguaje" >
@@ -61,7 +63,6 @@ export const SearchWeather = () => {
         <option value="sp">Spanish</option>
       </Form.Select>
     </Form.Group>
-    <Button variant="primary" type="submit">Enviar</Button>
           </Form>
 
           <SearchGeoCoding ciudad={Ciudad}  resultados={parseInt(Resultados)} lenguage={Lenguaje}/>
